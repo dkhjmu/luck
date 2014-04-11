b6677ac4caa702084fadc5f5dbdbc2d8a2cc8089
dkhjmu <df93@naver.com>
2014-04-11 11:20:36 +0900

    슬슬

--- src/a/ana/pattern/PatternMaker.java
+++ src/a/ana/pattern/PatternMaker.java
@@ -6,8 +6,13 @@ import a.act.main.vo.IntVO;
 
 public class PatternMaker {
 	public static void main(String[] args) {
+		System.out.println("Start Making!");
 		int t[]={1,2,3,4,5,6};
 		
+		System.out.println(t.length);
+		
 		HashMap<String, IntVO> map = new HashMap<String, IntVO>();
+		
+		System.out.println("End Making");
 	}
 }
