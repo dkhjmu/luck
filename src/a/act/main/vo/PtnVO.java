1061faccf0f06fab8a54da1093e6eb709681f234
dkhjmu <df93@naver.com>
2014-04-14 18:01:48 +0900

    패턴

--- src/a/act/main/vo/PtnVO.java
+++ src/a/act/main/vo/PtnVO.java
@@ -1,5 +1,21 @@
 package a.act.main.vo;
 
-public class PtnVO {
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class PtnVO {
+	ArrayList<IntVO> list;
+	
+	public PtnVO() {
+		list=new ArrayList<IntVO>();
+	}
+	
+	public void add(IntVO vo){
+		list.add(vo);
+		Collections.sort(list);
+	}
+	
+	public ArrayList<IntVO> getList(){
+		return list;
+	}
 }
--- src/a/ana/pattern/PatternMaker.java
+++ src/a/ana/pattern/PatternMaker.java
@@ -1,18 +1,51 @@
 package a.ana.pattern;
 
-import java.util.HashMap;
+import java.util.Collections;
 
 import a.act.main.vo.IntVO;
+import a.act.main.vo.PtnVO;
 
 public class PatternMaker {
+	
 	public static void main(String[] args) {
 		System.out.println("Start Making!");
 		int t[]={1,2,3,4,5,6};
 		
 		System.out.println(t.length);
 		
-		HashMap<String, IntVO> map = new HashMap<String, IntVO>();
+		System.out.println(getAllpattern(6, 3));
+		
+		PtnVO no1=getPtnBase(t);
+		
 		
 		System.out.println("End Making");
 	}
+	
+	
+	private static PtnVO getPtnBase(int[] t) {
+		PtnVO vo=new PtnVO();
+		for(int i=0;i<t.length;i++){
+			vo.add(new IntVO(t[i]));
+		}
+		return vo;
+	}
+
+
+	public static int getAllpattern(int maxSize, int picks){
+		int up = 1;
+		int down = 1;
+		
+		for(int i=0;i<picks;i++){
+			up = up * (maxSize - i);
+		}
+		
+		for(int i=0;i<picks;i++){
+			down = down * (picks - i);
+		}
+		
+		return (up / down);
+	}
+	
+	
+	
 }
