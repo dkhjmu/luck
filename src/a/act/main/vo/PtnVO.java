90d5f43e8c6686604135c4a371c8d00c8df9c002
dkhjmu <df93@naver.com>
2014-04-16 18:44:33 +0900

    패턴추가

--- src/a/act/main/vo/PtnVO.java
+++ src/a/act/main/vo/PtnVO.java
@@ -2,20 +2,69 @@ package a.act.main.vo;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 
 public class PtnVO {
 	ArrayList<IntVO> list;
 	
+	HashMap<String, IntVO> result; 
+	
+	int currentCnt = 0;
+
+	int pickCnt = 0;
+	
 	public PtnVO() {
 		list=new ArrayList<IntVO>();
+		result=new HashMap<String, IntVO>();
 	}
 	
-	public void add(IntVO vo){
-		list.add(vo);
+	public boolean add(IntVO vo){
+		boolean result=list.add(vo);
 		Collections.sort(list);
+		return result;
+	}
+	
+	public boolean remove(IntVO vo){
+		boolean result=list.remove(vo);
+		Collections.sort(list);
+		return result;
 	}
 	
 	public ArrayList<IntVO> getList(){
 		return list;
+	}
+	
+	public boolean isIn(String key){
+		IntVO vo = result.get(key);
+		if(vo==null){
+			return false;
+		}else{
+			return true;
+		}
+	}
+	
+	public boolean addResult(String key){
+		if(isIn(key)){
+			return false;
+		}
+		result.put(key, new IntVO(1));
+		
+		return true;
+	}
+
+	public int getCurrentCnt() {
+		return currentCnt;
+	}
+
+	public void setCurrentCnt(int currentCnt) {
+		this.currentCnt = currentCnt;
+	}
+
+	public int getPickCnt() {
+		return pickCnt;
+	}
+
+	public void setPickCnt(int pickCnt) {
+		this.pickCnt = pickCnt;
 	}
 }
--- src/a/ana/pattern/PatternMaker.java
+++ src/a/ana/pattern/PatternMaker.java
@@ -1,36 +1,20 @@
 package a.ana.pattern;
 
-import java.util.Collections;
+import java.util.ArrayList;
 
 import a.act.main.vo.IntVO;
 import a.act.main.vo.PtnVO;
 
 public class PatternMaker {
 	
-	public static void main(String[] args) {
-		System.out.println("Start Making!");
-		int t[]={1,2,3,4,5,6};
-		
-		System.out.println(t.length);
-		
-		System.out.println(getAllpattern(6, 3));
-		
-		PtnVO no1=getPtnBase(t);
-		
-		
-		System.out.println("End Making");
-	}
-	
-	
-	private static PtnVO getPtnBase(int[] t) {
-		PtnVO vo=new PtnVO();
-		for(int i=0;i<t.length;i++){
-			vo.add(new IntVO(t[i]));
-		}
-		return vo;
-	}
-
-
+	/**
+	 * 
+	 * 모든 타입의 갯수 세기
+	 * 
+	 * @param maxSize
+	 * @param picks
+	 * @return
+	 */
 	public static int getAllpattern(int maxSize, int picks){
 		int up = 1;
 		int down = 1;
@@ -47,5 +31,65 @@ public class PatternMaker {
 	}
 	
 	
+	public static void main(String[] args) {
+		System.out.println("Start Making!");
+		int t[]={1,2,3,4,5,6};
+		
+		//System.out.println(getAllpattern(6,1));
+		
+		PtnVO no1=getPtnBase(t);
+
+		no1.setPickCnt(2);
+		
+		getPtn(no1);
+		
+		
+		System.out.println("End Making");
+	}
+	
+	
+	private static void getPtn(PtnVO base) {
+		base.setCurrentCnt(base.getCurrentCnt()+1);
+		ArrayList<IntVO> list = base.getList();
+		
+		if(base.getCurrentCnt()==base.getPickCnt()){
+			if(list.size()==1){
+				System.out.println(list.get(0)+"\t");
+				return;
+			}else{
+				System.out.println(list.get(0)+"\t");
+				System.out.println();
+				base.remove(list.get(0));
+				PtnVO child = getPtnBase(list);
+				child.setCurrentCnt(base.getCurrentCnt());
+				child.setPickCnt(base.getPickCnt());
+				getPtn(child);
+			}
+		}
+		
+	}
+
+
+	public static PtnVO getPtnBase(ArrayList<IntVO> t) {
+		PtnVO vo=new PtnVO();
+		for(int i=0;i<t.size();i++){
+			vo.add(new IntVO(t.get(i)));
+		}
+		return vo;
+	}
+	
+	public static PtnVO getPtnBase(int[] t) {
+		PtnVO vo=new PtnVO();
+		for(int i=0;i<t.length;i++){
+			vo.add(new IntVO(t[i]));
+		}
+		return vo;
+	}
+
+
+	
+	
+	
 	
 }
+ 
