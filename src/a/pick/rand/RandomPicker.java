527f43a4ab70e2985449059e8799d5fcb3d3113f
dkhjmu <df93@naver.com>
2014-07-04 14:22:20 +0900

    dd

--- src/a/pick/rand/RandomPicker.java
+++ src/a/pick/rand/RandomPicker.java
@@ -8,6 +8,9 @@ import a.pick.AbstractPicker;
 
 public class RandomPicker extends AbstractPicker {
 
+	
+	int tryN=100;
+	
 	public static int getRand(int max) {
 		int rand = (int) (Math.floor(Math.random() * 100) % max);
 		return rand;
@@ -18,11 +21,20 @@ public class RandomPicker extends AbstractPicker {
 		return super.getSize();
 	}
 
+	
+	public void setTryN(int n){
+		this.tryN=n;
+	}
+	
 	@Override
 	public int[][] pick(int seq) {
 		
 		ArrayList<ResultVO> list = AnaVOMain.getResultListNoBonus();
 		AnaVOMain.getAnaVOList(list, seq);
+		
+		
+		
+		
 		
 		return null;
 	}
